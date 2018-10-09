11622dcdd41524ddcf1e1be9ca56ccbe0a6d4cc4
jannireddyk@gmail.com <jannireddy@7>
2018-10-09 12:11:36 -0400

    changes related to pb_parts_final-selected.py

--- lib/globalPb.py
+++ lib/globalPb.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy import ndimage
 import cv2
 from multiscalePb import multiscalePb
+import matplotlib.pyplot as plt
+
 # from PIL import Image
 
 def globalPb(imgFile, outFile = '', rsz = 1.0):
@@ -13,7 +15,9 @@ def globalPb(imgFile, outFile = '', rsz = 1.0):
 	# im = Image.open(imgFile)
 	# im.load()
 	# data = np.asarray(im, dtype= "int64")
-	im = cv2.imread(imgFile)
+	bgr_im = cv2.imread(imgFile)
+	b,g,r = cv2.split(bgr_im)       # get b,g,r
+	im = cv2.merge([r,g,b])			# covert to r,g,b
 	im = im/255
 	[tx, ty, nchan] = im.shape
 	orig_sz = [tx, ty]
@@ -25,6 +29,14 @@ def globalPb(imgFile, outFile = '', rsz = 1.0):
 	[mPb, mPb_rsz, bg1, bg2, bg3, cga1, cga2, cga3, cgb1, cgb2, cgb3, tg1, tg2, tg3, textons] = multiscalePb(im, rsz)
 	print (orig_sz)
 	print (weights)
+	# # To show the image
+	# plt.imshow(im)
+	# plt.xticks([]), plt.yticks([])   # to hide tick values on X and Y axis
+	# plt.show()
+	# while True:
+	# 	k = cv2.waitKey(0) & 0xFF    # 0xFF? To get the lowest byte.
+	# 	if k == 27: break            # Code for the ESC key
+	# cv2.destroyAllWindows()
 	
 
 globalPb('../data/101087.jpg')
--- lib/pb_parts_final_selected.py
+++ lib/pb_parts_final_selected.py
@@ -1,4 +1,6 @@
+import cv2
 import math
+import numpy as np
 
 def pb_parts_final_selected(L, a, b):
     print ("inside pb_parts_final_selected function")
@@ -22,6 +24,106 @@ def pb_parts_final_selected(L, a, b):
     r_tg = [ 5, 10, 20 ]
 
     # compute bg histogram smoothing kernel
+
+
+    # mirror border
+    L = cv2.copyMakeBorder(L, border, border, border, border, cv2.BORDER_REFLECT)
+    a = cv2.copyMakeBorder(a, border, border, border, border, cv2.BORDER_REFLECT)
+    b = cv2.copyMakeBorder(b, border, border, border, border, cv2.BORDER_REFLECT)
+
+    # L = makeBorder(L, border)
+    # a = makeBorder(a, border)
+    # b = makeBorder(b, border)
+
+    # convertto grayscale
+    g_im = np.empty(L.shape)
+    g_im.dtype = L.dtype
+    g_im = (0.29894 * L) + (0.58704 * a) + (0.11402 * b)
+    # for i in range(0, L.shape[0]):
+    #     for j in range(0, L.shape[1]):
+    #         g_im[i][j] = (0.29894 * L[i][j]) + (0.58704 * a[i][j]) + (0.11402 * b[i][j])
+
+    # gamma correct
+    np.power(L, 2.5)
+    np.power(a, 2.5)
+    np.power(b, 2.5)
+
+    print (L.dtype)
+    print (a.dtype)
+    print (b.dtype)
+
+    # rgb_im_temp = cv2.merge([L,a,b])
+    # print (rgb_im_temp.shape)
+
+    # Convert to Lab color space
+    # dest_im = cv2.cvtColor(rgb_im_temp, cv2.COLOR_RGB2Lab)
+    # L,a,b = cv2.split(rgb_im_temp)
+    # print (rgb_im_temp.shape)
+    # print (L)
+    x_l = (0.412453 * L) +  (0.357580 * a) + (0.180423 * b)
+    y_a = (0.212671 * L) +  (0.715160 * a) + (0.072169 * b)
+    z_b = (0.019334 * L) +  (0.119193 * a) + (0.950227 * b)
+    # D65 white point reference
+    x_ref = 0.950456
+    y_ref = 1.000000
+    z_ref = 1.088754
+    # threshold value
+    threshold = 0.008856
+    # convert XYZ to Lab
+    for i in range(0, L.shape[0]):
+        for j in range(0, L.shape[1]):
+            x = x_l[i][j] / x_ref
+            y = y_a[i][j] / y_ref
+            z = z_b[i][j] / z_ref
+            # compute fx, fy, fz
+            if x > threshold:
+                fx = math.pow(x,(1.0/3.0))
+            else:
+                fx = (7.787*x + (16.0/116.0))
+
+            if y > threshold:
+                fy = math.pow(y,(1.0/3.0))
+            else:
+                fy = (7.787*y + (16.0/116.0))
+            
+            if z > threshold:
+                fz = math.pow(z,(1.0/3.0))
+            else:
+                fz = (7.787*z + (16.0/116.0))
+
+            # compute Lab color value
+            if y > threshold:
+                x_l[i][j] = (116*math.pow(y,(1.0/3.0)) - 16)
+            else:
+                x_l[i][j] = 903.3*y
+            
+            y_a[i][j] = 500 * (fx - fy)
+            z_b[i][j] = 200 * (fy - fz)
+    L = x_l
+    a = y_a
+    b = z_b
+    
     
 
-    # return [textons, bg_r3, bg_r5,  bg_r10,  cga_r5, cga_r10, cga_r20, cgb_r5, cgb_r10, cgb_r20, tg_r5,  tg_r10,  tg_r20]
+    # print (L)
+    # print (a)
+    # print (b)
+    # for i in range(0, L.shape[0]):
+    #     for j in range(0, L.shape[1]):
+    #         L[i][j] = pow(L[i][j], 2.5)
+    #         a[i][j] = pow(a[i][j], 2.5)
+    #         b[i][j] = pow(b[i][j], 2.5)
+
+    
+
+
+    print (L.shape)
+    print (a.shape)
+    print (b.shape)
+
+
+
+    return [0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0,  0,  0]
+
+# def makeBorder(L, border):
+#     pass
